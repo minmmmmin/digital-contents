1e4b75fb1349f4b95bc40c0428a0de89bf9a4669
s5422053 <chda22053@g.nihon-u.ac.jp>
2025-12-04 09:22:22 +0900

    feature: #14 タイムライン拡大・縮小時のタブバーの移動をアニメーション化

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -15,21 +15,23 @@ export default function Home() {
       <Header />
       <div className="flex-1 flex flex-col min-h-0">
         <div
-          className={
-            view === 'split' ? 'h-1/2' :
-            view === 'map' ? 'flex-1' : 'hidden'
-          }
+          className={`transition-all duration-300 ease-in-out overflow-hidden ${
+            view === 'split' ? 'h-1/2' : view === 'map' ? 'flex-1' : 'h-0'
+          }`}
         >
           <Map view={view} setView={setView} />
         </div>
-        <div className={view === 'map' ? 'hidden' : ''}>
+        <div className={`${view === 'map' ? 'hidden' : ''}`}>
           <TabsBar />
         </div>
         <div
-          className={
-            view === 'split' ? 'flex-1 min-h-0' :
-            view === 'timeline' ? 'flex-1 min-h-0' : 'hidden'
-          }
+          className={`transition-all duration-300 ease-in-out overflow-hidden ${
+            view === 'split'
+              ? 'flex-1 min-h-0'
+              : view === 'timeline'
+              ? 'flex-1 min-h-0'
+              : 'h-0'
+          }`}
         >
           <Timeline view={view} setView={setView} />
         </div>

0deddbc2e42fffee3d1513a57382e7f3a870d53c
minmmmmin <kotomi.hinahima@gmail.com>
2025-12-16 16:26:40 +0900

    fix: デプロイ前の修正

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,26 +1,37 @@
-import { createMiddlewareClient } from '@supabase/ssr'
-import { NextResponse } from 'next/server'
-
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { createServerClient } from "@supabase/ssr";
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next()
-  const supabase = createMiddlewareClient({ req: request, res: response })
+  const response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
 
-  await supabase.auth.getSession()
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options);
+          });
+        },
+      },
+    }
+  );
 
-  return response
+  // セッションを読み込むだけ（今までの挙動と同じ）
+  await supabase.auth.getSession();
+
+  return response;
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    '/((?!_next/static|_next/image|favicon.ico).*)',
-  ],
-}
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
